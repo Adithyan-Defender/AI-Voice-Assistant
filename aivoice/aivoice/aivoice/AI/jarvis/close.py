e5f541a7701974b746f39f1b4b99d25f80c121fe
Adithyan-Defender <itsadithyanm@gmail.com>
2025-09-10 23:29:49 +0530

    Delete aivoice/aivoice directory

--- aivoice/aivoice/aivoice/AI/jarvis/close.py
+++
@@ -1,9 +0,0 @@
-import random
-import pyautogui as ui
-from DLG import closedlg
-from Mouth import speak
-
-closedlg_random = random.choice(closedlg)
-def close():
-    speak(closedlg_random)
-    ui.hotkey("alt","f4")
--- aivoice/aivoice/aivoice/web/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ChatMessage,UploadedImage
-
-admin.site.register(ChatMessage),
-
-admin.site.register(UploadedImage)
